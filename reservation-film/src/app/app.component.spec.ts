8184a2b016ac2370bec44199fe5c94db92be80a1
Moussa Traore <moussatraore@MacBook-Pro-de-Moussa.local>
2025-11-07 15:08:53 +0100

    creation du projet

--- reservation-film/src/app/app.component.spec.ts
+++ reservation-film/src/app/app.component.spec.ts
@@ -1,12 +1,18 @@
 import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [RouterTestingModule],
-    declarations: [AppComponent]
-  }));
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [
+        RouterModule.forRoot([])
+      ],
+      declarations: [
+        AppComponent
+      ],
+    }).compileComponents();
+  });
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
@@ -14,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'movie-angular'`, () => {
+  it(`should have as title 'reservation-film'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('movie-angular');
+    expect(app.title).toEqual('reservation-film');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('movie-angular app is running!');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, reservation-film');
   });
 });
--- reservation-film/src/app/app.component.ts
+++ reservation-film/src/app/app.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  standalone: false,
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'movie-angular';
+  title = 'reservation-film';
 }
